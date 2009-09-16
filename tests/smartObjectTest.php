8e9f0c69c587092944177e3bbe43f2ec46e0c24d
Bryan P. Johnson <bryan@12ftguru.com>
2009-09-16 14:17:42 -0400

    Tests and more tests.
    Signed-off-by: Bryan P. Johnson <bryan@12ftguru.com>

--- tests/smartObjectTest.php
+++ tests/smartObjectTest.php
@@ -1,6 +1,19 @@
 <?php
 
-/* Requires go here */
+error_reporting(E_ALL ^ E_NOTICE);
+include_once('../alib.inc');
+addIncludePath('../');
+addIncludePath('mockery/library', TRUE);
+include_once('Mockery.php');
+include_once('Mockery/Framework.php');
+//include_once('idb.inc');
+
+/* Can't test an abstract class directly. */
+
+$db = Mockery::mock('idb');
+
+
+class testSmartObject extends smartObject {};
 
 /**
  * Test class for smartObject.
@@ -22,7 +35,9 @@ class smartObjectTest extends PHPUnit_Framework_TestCase
      */
     protected function setUp()
     {
-        $this->object = new smartObject;
+      global $db;
+      $this->object = new testSmartObject();
+      $this->object->db = $db;
     }
 
     /**
@@ -33,6 +48,9 @@ class smartObjectTest extends PHPUnit_Framework_TestCase
      */
     protected function tearDown()
     {
+      
+      unset($this->object->db);
+
     }
 
     /**
@@ -40,10 +58,8 @@ class smartObjectTest extends PHPUnit_Framework_TestCase
      */
     public function test__set()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
+      $this->object->foo = 'bar';
+      $this->assertEquals('bar', $this->object->vars['foo']);
     }
 
     /**
@@ -51,10 +67,8 @@ class smartObjectTest extends PHPUnit_Framework_TestCase
      */
     public function test__get()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
+      $this->object->foo = 'bar';
+      $this->assertEquals('bar', $this->object->foo);
     }
 
     /**
@@ -62,21 +76,22 @@ class smartObjectTest extends PHPUnit_Framework_TestCase
      */
     public function test__toString()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
-    }
+      $this->object->foo = 'bar';
+      ob_start();
+      print $this->object;
+      $string = ob_get_contents();
+      ob_end_clean();
+      $this->assertEquals('{"foo":"bar","sotype":"testSmartObject"}', $string);
+   }
 
     /**
      * @todo Implement testReset().
      */
     public function testReset()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
+      $this->object->foo = 'bar';
+      $this->object->reset();
+      $this->assertEquals(NULL, $this->object->vars['foo']);
     }
 
     /**
@@ -84,22 +99,85 @@ class smartObjectTest extends PHPUnit_Framework_TestCase
      */
     public function testRead()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
+      $this->object->testSmartObjectID = 1;
+
+      $retval = array('testSmartObjectID' => 1, 'column1' => 'value1', 'column2' => 'value2');
+
+      $testResult = mockery('result', array('fetch_assoc' => $retval));
+      $testResult->num_rows = 1;
+
+
+      $this->object->db->shouldReceive('real_escape_string')->withAnyArgs()->zeroOrMoreTimes()->andReturn(1);
+      $this->object->db->shouldReceive('query')->withArgsMatching("/select \* from \`testsmartobjects\` where \`testSmartObjectID\` = \'1\'/")->once()->andReturn($testResult);
+
+      /* This is the actual test */
+      $this->object->read();
+      /* And that's that. */
+
+
+      mockery_verify();
+
+      $this->assertEquals(3, count($this->object->vars));
+      $this->assertEquals('value1', $this->object->column1);
+      $this->assertEquals('value2', $this->object->column2);
     }
 
     /**
-     * @todo Implement testInsert().
+     * @todo Implement testInsertSuccess().
      */
-    public function testInsert()
+    public function testInsertSuccess()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
+
+
+
+
+      $this->object->db->shouldReceive('real_escape_string')->withAnyArgs()->zeroOrMoreTimes()->andReturnArgs();
+      $this->object->db->shouldReceive('query')->withArgsMatching("/insert into \`testsmartobjects\` \(foo\) values \(\'bar\'\)/")->once()->andReturn(TRUE);
+      $this->object->db->insert_id = 1;
+      $this->object->db->errno = 0;
+      $this->object->db->error = 'Success';
+
+      $this->object->foo = 'bar';
+
+      $retval = $this->object->insert();
+
+      mockery_verify();
+
+      $this->assertEquals(1, $retval);
+      $this->assertEquals('Success', $this->object->error);
+
     }
+
+
+    /**
+     * @todo Implement testInsertFailure().
+     */
+    public function testInsertFailure()
+    {
+
+
+      $this->object->db->insert_id = NULL;
+      $this->object->db->errno = 1062;
+      $this->object->db->error = "Duplicate entry '1' for key 'idnum'";
+
+
+      $this->object->db->shouldReceive('real_escape_string')->withAnyArgs()->zeroOrMoreTimes()->andReturnArgs();
+      $this->object->db->shouldReceive('query')->withArgsMatching("/insert into \`testsmartobjects\` \(testSmartObjectID,foo\) values \('1', \'bar\'\)/")->once()->andReturn(TRUE);
+
+      $this->object->id = 1;
+      $this->object->foo = 'bar';
+
+
+      $retval = $this->object->insert();
+
+      mockery_verify();
+
+      $this->assertEquals(FALSE, $retval);
+      $this->assertEquals(1062, $this->object->errno);
+
+    }
+
+
 
     /**
      * @todo Implement testUpdate().
